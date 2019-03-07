82c860760e2d9eae0f099ed2133488c60f8065c3
SatyendraSingh <satyendra.singh3339@gmail.com>
2019-03-08 00:29:31 +0530

    python practice

---
+++ in/ss/python/examples/problem_1.py
@@ -0,0 +1,5 @@
+numbers = [1, 3, 2, 4]
+print(numbers[1])
+
+
+def add_one(numbers)
---
+++ in/ss/python/examples/variables.py
@@ -0,0 +1,12 @@
+v1 = "first string"
+v2 = "second string"
+
+print("v1 : " + v1)
+print("v2 : " + v2)
+
+temp = v1
+v1 = v2
+v2 = temp
+
+print("v1 : " + v1)
+print("v2 : " + v2)
